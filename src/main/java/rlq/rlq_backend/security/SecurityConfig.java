769e33db27d985b6ce5ca811c81ef80ff8741e87
Brhom <bhm@gmail.com>
2025-12-30 23:13:24 +0300

    asd

--- src/main/java/rlq/rlq_backend/security/SecurityConfig.java
+++ src/main/java/rlq/rlq_backend/security/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("https://rlq.life"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setExposedHeaders(List.of("Authorization")); // optional
